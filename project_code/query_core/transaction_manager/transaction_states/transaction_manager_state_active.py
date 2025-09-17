f1ca5cd524b7f67bdaa18ac7bc010b6bc986a4f3
kichiro-kun <210242810+kichiro-kun@users.noreply.github.com>
2025-09-17 17:10:58 +0300

    Разработка классов состояний
    Major changes:
    - Edit `transaction_manager_state_active.py` (./project_code/query_core/transaction_manager/transaction_states/);
    - Edit `transaction_manager_state_committed.py` (./project_code/query_core/transaction_manager/transaction_states/);
    - Edit `transaction_manager_state_initialized.py` 0.1.0 -> 0.2.0 (./project_code/query_core/transaction_manager/transaction_states/);
    - Edit `transaction_manager_state_rolledback.py` (./project_code/query_core/transaction_manager/transaction_states/);
    - Edit `test_transaction_manager_states.py` 0.1.0 -> 0.2.0 (./project_code/tests/test_query_core/);
    
    *Importantly:
    Coverage Report - 99%

--- project_code/query_core/transaction_manager/transaction_states/transaction_manager_state_active.py
+++ project_code/query_core/transaction_manager/transaction_states/transaction_manager_state_active.py
@@ -1,24 +1,50 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright 2025 kichiro-kun (Kei)
+Apache license, version 2.0 (Apache-2.0 license)
+"""
+
+__author__ = 'kichiro-kun (Kei)'
+__version__ = '0.1.0'
+
+# =======================================================================================
 from query_core.transaction_manager.abstract.transaction_state_interface \
     import TransactionStateInterface
-from typing import TYPE_CHECKING
 
+from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from query_core.transaction_manager.transaction_manager \
         import TransactionManager
+    from query_core.transaction_manager.transaction_states.transaction_manager_state_committed \
+        import TransactionManagerStateCommitted
 
 
+# _______________________________________________________________________________________
 class TransactionManagerStateActive(TransactionStateInterface):
-    def __init__(self, transaction_manager: 'TransactionManager') -> None:
-        pass
 
+    # -----------------------------------------------------------------------------------
+    def __init__(self, transaction_manager: 'TransactionManager') -> None:
+        self.root: 'TransactionManager' = transaction_manager
+
+    # -----------------------------------------------------------------------------------
     def begin(self) -> None:
         return
 
+    # -----------------------------------------------------------------------------------
     def execute_in_active_transaction(self) -> None:
         return
 
+    # -----------------------------------------------------------------------------------
     def commit(self) -> None:
-        return
+        next_state: 'TransactionManagerStateCommitted' = self.root.committed_state
 
+        # Set next state
+        self.root.set_state(new_state=next_state)
+
+        # Delegate operation to next state
+        self.root.commit()
+
+    # -----------------------------------------------------------------------------------
     def rollback(self) -> None:
         return
--- project_code/tests/test_query_core/test_transaction_manager_states.py
+++ project_code/tests/test_query_core/test_transaction_manager_states.py
@@ -6,10 +6,9 @@ Apache license, version 2.0 (Apache-2.0 license)
 """
 
 __author__ = 'kichiro-kun (Kei)'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 # =======================================================================================
-from os import path
 from unittest import TestCase, mock as UM
 
 import query_core.transaction_manager.transaction_manager as tested_module
@@ -54,12 +53,90 @@ class TestComponentPositive(TestCase):
 
     # -----------------------------------------------------------------------------------
     def test_active_state_behavior_next_state_logic(self) -> None:
-        pass
+        # Build
+        transaction_manager = self.get_instance_of_transaction_manager()
+
+        # Extract
+        active_state = transaction_manager.active_state
+        committed_state = transaction_manager.committed_state
+
+        # Prepare transaction manager
+        transaction_manager.set_state(new_state=active_state)
+
+        # Prepare pre-check context
+        with UM.patch.object(target=active_state,
+                             attribute='execute_in_active_transaction') as mock_execute_in_active_transaction:
+            # Operate
+            transaction_manager.execute_in_active_transaction()
+
+            # Pre-check
+            mock_execute_in_active_transaction.assert_called_once()
+
+        # Prepare check context
+        with UM.patch.object(target=committed_state,
+                             attribute='commit') as mock_method_commit:
+            # Operate
+            transaction_manager.commit()
+
+            # Check
+            mock_method_commit.assert_called_once()
 
     # -----------------------------------------------------------------------------------
     def test_committed_state_behavior_next_state_logic(self) -> None:
-        pass
+        # Build
+        transaction_manager = self.get_instance_of_transaction_manager()
+
+        # Extract
+        committed_state = transaction_manager.committed_state
+        rolledback_state = transaction_manager.rolledback_state
+
+        # Prepare transaction manager
+        transaction_manager.set_state(new_state=committed_state)
+
+        # Prepare pre-check context
+        with UM.patch.object(target=committed_state,
+                             attribute='commit') as mock_method_commit:
+            # Operate
+            transaction_manager.commit()
+
+            # Pre-check
+            mock_method_commit.assert_called_once()
+
+        # Prepare check context
+        with UM.patch.object(target=rolledback_state,
+                             attribute='rollback') as mock_method_rollback:
+            # Operate
+            transaction_manager.rollback()
+
+            # Check
+            mock_method_rollback.assert_called_once()
 
     # -----------------------------------------------------------------------------------
     def test_rolledback_state_behavior_next_state_logic(self) -> None:
-        pass
+        # Build
+        transaction_manager = self.get_instance_of_transaction_manager()
+
+        # Extract
+        rolledback_state = transaction_manager.rolledback_state
+        initialized_state = transaction_manager.initialized_state
+
+        # Prepare transaction manager
+        transaction_manager.set_state(new_state=rolledback_state)
+
+        # Prepare pre-check context
+        with UM.patch.object(target=rolledback_state,
+                             attribute='rollback') as mock_method_rollback:
+            # Operate
+            transaction_manager.rollback()
+
+            # Pre-check
+            mock_method_rollback.assert_called_once()
+
+        # Prepare check context
+        with UM.patch.object(target=initialized_state,
+                             attribute='begin') as mock_method_begin:
+            # Operate
+            transaction_manager.begin()
+
+            # Check
+            mock_method_begin.assert_called_once()
